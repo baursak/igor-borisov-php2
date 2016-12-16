d7336a2b615ad95f65079a13428df57098e4557f
baursak <baursak@gmail.com>
2016-12-16 12:07:35 +0300

    day 1 - 01:01:36: serialize / unserialize and base64_encode / base64_decode

--- demo/cookies/cookie.php
+++ demo/cookies/cookie.php
@@ -1,3 +1,11 @@
 <?php
-	setcookie("userName", 'John');
+//setcookie( "userName", 'John' );
+//echo $_COOKIE["userName"];
+$user = [
+	'name' => 'John',
+	'login' => 'root',
+	'password' => '1234',
+];
+$str = serialize($user);
+echo base64_encode($str);
 ?>
