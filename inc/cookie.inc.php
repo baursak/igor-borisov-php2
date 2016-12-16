4f8d0900c64f1928090af8d6082a454bf513c6a6
baursak <baursak@gmail.com>
2016-12-16 13:26:43 +0300

    day 1 - 01:12:39: 'visit counter' example

--- inc/cookie.inc.php
+++ inc/cookie.inc.php
@@ -1,1 +1,16 @@
 <?
+// разбираемся с количеством предыдущих посещений нашего сайта пользователем
+$visitCounter = 0;
+if (isset($_COOKIE['visitCounter'])) {
+	$visitCounter = $_COOKIE['visitCounter'];
+}
+$visitCounter ++;
+
+// разбираемся с датой последнего посещения нашего сайта пользователем
+$lastVisit = "";
+if (isset($_COOKIE['lastVisit'])) {
+	$lastVisit = date('Y-m-d H:i:s', $_COOKIE['lastVisit']);
+}
+
+setcookie('visitCounter', $visitCounter);
+setcookie('lastVisit', time());
